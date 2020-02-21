1d522ac5be74927670f90ee01068ccc36d0afa97
moodooTech <moodootechnology@gmail.com>
2020-02-20 21:51:00 -0800

    particle background

--- bkg_js/bkg_js.js
+++ bkg_js/bkg_js.js
@@ -2,27 +2,27 @@ var angle =0.0;
 var diameter = 0.0;
 var  n = 256;//
 var minRad = 25;
-var maxRad = 500;
+var maxRad = 300;
 var nfAng = 0.0289;//change this to get new shape
 var  nfTime =0.0137;
 function setup() {
   createCanvas(displayWidth, displayHeight);
-  background(0); 
+  background(255); 
   //colorMode(HSB);
   smooth();
 } 
 
 function draw() {
   var j =0;
-  background(255);
+ // background(255);
   translate(displayWidth/2, displayHeight/2);
+  scale(0.5);
 //noFill();
   beginShape();
   //push();
   for (var i =0; i< displayWidth; i+=0.1) {  
    // drawFlower(-i, i, 50);
     //rotate(sin(i+mouseX)*cos(i+mouseX)+cos(mouseX/10));
-
 
    
     var ang = map(i, 0, n, 0, 3*PI);
@@ -31,12 +31,12 @@ function draw() {
     var y = rad * tan(ang);
     //  drawFlower(x, y, 15);
    // fill(255);
-   alpha(120);
+   alpha(150);
     stroke(i,100,100);
     strokeWeight(0.2);
     //nnoStroke();
      noFill();
-   arc(noise(mouseX,x*2), y*2, i/2,i/2,PI,PI*3);
+   arc(noise(mouseX,x*2), noise(mouseY,y*2), i/2,i/2,PI,PI*3);
  arc(x, y, i/2,i/2,HALF_PI,PI*i);
  // arc(-x, -y, i,i,HALF_PI,PI*3);
      // arc(x+i, y+i, i,i,HALF_PI,PI*3);
--- js/bkg_js.js
+++ js/bkg_js.js
@@ -19,39 +19,41 @@ function draw() {
   background(255);
 
   translate(width/2,height/2);
-   scale(0.2);
+  scale(0.1);
 //noFill();
   beginShape();
 
   //push();
-  for (var i =0; i< displayWidth/4; i+=0.1) {  
+  for (var i =0; i< displayWidth/4; i+=0.2) {  
    // drawFlower(-i, i, 50);
     //rotate(sin(i+mouseX)*cos(i+mouseX)+cos(mouseX/10));
 
 
    
     var ang = map(i, 0, n, 0, 3*PI);
-    var rad = map(noise(i*nfAng, frameCount*nfTime), 0, 2.1, minRad, maxRad);
+    var rad = map(noise(i*nfAng, frameCount/2*nfTime), 0, 2.1, minRad, maxRad);
     var x = rad * cos(ang);
     var y = rad * sin(ang);
     //  drawFlower(x, y, 15);
    // fill(255);
-   alpha(180);
-    stroke(i,150,150);
-    strokeWeight(0.5);
+   alpha(200);
+    stroke(i,100,150);
+    strokeWeight(2.2);
     //nnoStroke();
      noFill();
-  // arc(x*5, y*5, i/2,i/2,HALF_PI,PI*2);
+  
  //arc(mouseX/2, mouseY/2, i/2,i/2,HALF_PI,PI*i);
-  arc(-x*i,y*i,i,i,HALF_PI,PI*3);
+  //arc(-x*i,y*i,i,i,HALF_PI,PI*3);
      // arc(x+i/3, y+i/3, i,i,HALF_PI,PI*3);
          //   arc(x+i/2, y+i/2, i,i,PI,PI*3);
-  arc(-x+i, y+i, i,i,PI,PI*2);
-rotate(mouseX*0.1);
+         ellipse(x*i,y*i,25,25);
+        // fill(i,i,150,20);
+  //arc(-x*i, y*i, i,i,PI,PI*2);
+rotate(tan(pmouseX+x/10000000));
 
   }
   endShape();
   
  //pop();
-  angle+=0.00001*mouseX;
+ arc(x*i, -y*i, i/2,i/2,HALF_PI,PI*2);
 }
